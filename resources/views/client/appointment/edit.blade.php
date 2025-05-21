13a42de222d5aed212b366a78d328964265d173c
rizkimaulana32 <sijond34@gmail.com>
2025-05-21 23:26:37 +0700

    fix: simulate hotfix crash client edit appointment page

--- resources/views/client/appointment/edit.blade.php
+++ resources/views/client/appointment/edit.blade.php
@@ -5,6 +5,7 @@
         </h2>
     </x-slot>
 
+    <!-- Simulasi Hotfix edit appointment -->
     <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
